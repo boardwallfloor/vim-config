73db934bb9e4df4c8bcee0dfec49c3d306d64df5
Faris T Z <xanthibumi@gmail.com>
2024-12-22 18:02:21 +0700

    stuff

--- init.lua
+++ init.lua
@@ -973,6 +973,15 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -1020,8 +1029,11 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
+-- Maintain cursor style in terminal from becoming block
 vim.api.nvim_create_autocmd('ExitPre', {
   group = vim.api.nvim_create_augroup('Exit', { clear = true }),
   command = 'set guicursor=a:ver90',
   desc = 'Set cursor back to beam when leaving Neovim.',
 })
+
+-- Vimtex setup
